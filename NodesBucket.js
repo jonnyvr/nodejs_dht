eee2d0581c9aef1aab63f9674acc47cff0d93f5d
zj <zj@szxlzkj.com>
2016-08-17 14:09:15 +0800

    wwwww

--- NodesBucket.js
+++ NodesBucket.js
@@ -55,7 +55,7 @@ class NodesBucket {
 					tmp_dis += tmp_dis_arr[i]<<(8*(19-i));
 				}
 				
-				console.log(this._data);
+				console.log(this._bucketIndex);
 				console.log(srcNode._nodeId);
 				console.log(targetNode._nodeId);
 //				console.log(tmp_dis_arr);
