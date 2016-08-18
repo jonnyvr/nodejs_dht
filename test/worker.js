ce44ffa4b6bfbaad8b35921af39806cd005e3ef7
zj <zj@szxlzkj.com>
2016-08-18 17:28:18 +0800

    www

--- test/worker.js
+++ test/worker.js
@@ -2,6 +2,4 @@
 const Dht = require('./dht');
 const config = require('./config');
 
-//const dht = new Dht(config.port, config.address);
-var tmp_var = 0x55;
-console.log(tmp_var.toString("hex"));
+const dht = new Dht(config.port, config.address);
